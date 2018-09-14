a022c9c0c7d93cdd14c2a7baf5bc3e3affa803bf
Samuel Watkins <samwatkins@galba.stanford.edu>
2018-09-14 13:06:46 -0700

    Sam's test of GitPython

--- git_test2.py
+++ git_test2.py
@@ -2,9 +2,9 @@ import os
 import git
 
 
-repo = git.Repo('/scratch/cwfink/repositories/analysisTools')
+repo = git.Repo(os.path.dirname(os.path.abspath(__file__)))
 repo.git.add("git_test2.py")
-repo.git.commit(m = "trying to test GitPython")
+repo.git.commit(m = "Sam's test of GitPython")
 #print(repo.git.status())
 
 repo.git.pull('origin', 'master')
