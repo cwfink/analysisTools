7846aaf97cbd994c300b0a5cab39f7f9966422e1
cwfink <cwfink@berkeley.edu>
2018-09-16 14:44:00 -0700

    wrote cuts.py as a class CutUtils

--- cuts.py
+++ cuts.py
@@ -50,209 +50,256 @@ def calcbaselinecut(arr, r0, i0, rload, dr = 0.1e-3, cut = None):
     
     return cbase_pre
 
-
-
-def savecut(cutarr, name, description):
+class CutUtils(object):
     """
-    Function to save cut arrays. The function first checks the current_cut/ directory
-    to see if the desired cut exists. If not, the cut is saved. If the current cut does
-    exist, the fuction checkes if the cut has changed. If it has, the old version is archived
-    and the new version of the cut is saved. If nothing in the cut has changed, nothing is done.
+    Class to manage the saveing, loading, and archiving of cut masks for a decector analysis/DM search. 
+    This class keeps track of the most current version of user defined cuts, and archives previous versions
+    of the same cut, saving all versions. A user message is also saved with each cut, where the user provides
+    a short description of how the cut was generatred. This class will create the directory structure to save 
+    all the cuts, and if working with multiple people, this class will also automatically backup the directory
+    structure, and cut masks, on the shared github repository. Every time a cut is saved or loaded, this class
+    will make sure the data on the repository, and the local machine are up to with each other.
     
-    Cut arrays are saved as .npz files, with keys: 'cut' -> the cut array, and 
-    'cutdescription' -> a short message about how the cut was calculated.
+    This class assumes that it is being run inside the remote github repository directory. 
     
-    Note, the function expects current cuts to be in directory: current_cuts/, and archived cuts
-    to be in directory: archived_cuts/. If these folders do not exist in the given path, they will
-    be created by save_cut()
+    Note, if the automatic syncing with the repository functionality is to be used, the user must provide git 
+    with thier ssh key, as well as clone the repository using ssh protocal, not HTTP
     
-    Parameters
+    
+    Attributes
     ----------
-        cutarr: ndarray
-            Array of bools
-        name: str
-            The name of cut to be saved
-        description: str
-            Very short description of how the cut was calculated
-            
-    Returns
-    -------
-        None
-    
+    relativepath: str
+        This is the user supplied path to create the cut mask directory. This will be a relative path 
+        within the repository. For example, if working on an analysis for a particular run and dataset, 
+        set relativepath = 'Run44_dataset1'. Note, this path need not exist before creation of the CutUtils 
+        object, it will be created upon instantiation.
+    fullpath: str
+        The absolute path to the save path for the cut masks. It will be the absolute path to the repository 
+        where the analysis is being done, plus the relativepath
+        
+        
     """
-    path = os.path.dirname(os.path.abspath(__file__))
     
-    # check if 'current_cuts/' and 'archived_cuts/' exist. If not, make them
-    if not os.path.isdir(f'{path}/current_cuts'):
-        print('folder: current_cuts/ does not exist, it is being created now')
-        os.makedirs(f'{path}/current_cuts')
-    if not os.path.isdir(f'{path}/archived_cuts'):
-        print('folder: archived_cuts/ does not exist, it is being created now')
-        os.makedirs(f'{path}/archived_cuts')
-    
-    # check if there is a current cut, then check if it has been changed
-    try:
-        ctemp = np.load(f'{path}/current_cuts/{name}.npz')['cut']
+    def __init__(self,relativepath):
+        """
+        Initialization of the CutUtils object. If the directory structure to save the cuts is not already in 
+        place, they will be created upon instantiation of a CutUtils object
         
-        if np.array_equal(ctemp, cutarr):
-            print(f'cut: {name} is already up to date.')
-        else:
-            print(f'updating cut: {name} in directory: {path}/current_cuts/ and achiving old version')
-            np.savez_compressed(f'{path}/current_cuts/{name}', cut = cutarr, cutdescription=description)
+        Parameters
+        ----------
+            relativepath: str
+                This is the user supplied path to create the cut mask directory. This will be a relative path 
+                within the repository. For example, if working on an analysis for a particular run and dataset, 
+                set relativepath = 'Run44_dataset1'. Note, this path need not exist before creation of the CutUtils 
+                object, it will be created upon instantiation.
+                
+        """
+        if relativepath[0] != '/':
+            relativepath = '/' + relativepath
+        if relativepath[-1] == '/':
+            relativepath = relativepath[:-1]
             
-            files_old = glob(f'{path}/archived_cuts/{name}_v*')
-            if len(files_old) > 0:
-                latestversion = int(sorted(files_old)[-1].split('_v')[-1].split('.')[0])
-                version = int(latestversion +1)
-            else:
-                version = 0
-            np.savez_compressed(f'{path}/archived_cuts/{name}_v{version}', cut = ctemp, cutdescription=description)
-            print(f'old cut is saved as: {path}/archived_cuts/{name}_v{version}.npz')
+        self.relativepath = relativepath
+        self.fullpath = os.path.dirname(os.path.abspath(__file__)) + self.relativepath
         
-    except FileNotFoundError:
-        print(f'No existing version of cut: {name}. \n Saving cut: {name}, to directory: current_cuts/')
-        np.savez_compressed(f'{path}/current_cuts/{name}', cut = cutarr, cutdescription=description)
-        
-    return
+        if not os.path.isdir(f'{self.fullpath}/current_cuts'):
+            print(f'folder: {self.relativepath}/current_cuts/ does not exist, it is being created now')
+            os.makedirs(f'{self.fullpath}/current_cuts')
+        if not os.path.isdir(f'{self.fullpath}/archived_cuts'):
+            print(f'folder: {self.relativepath}/archived_cuts/ does not exist, it is being created now')
+            os.makedirs(f'{self.fullpath}/archived_cuts')
+
+    def savecut(self,cutarr, name, description):
+        """
+        Function to save cut arrays. The function first checks the current_cut/ directory
+        to see if the desired cut exists. If not, the cut is saved. If the current cut does
+        exist, the fuction checkes if the cut has changed. If it has, the old version is archived
+        and the new version of the cut is saved. If nothing in the cut has changed, nothing is done.
+
+        Cut arrays are saved as .npz files, with keys: 'cut' -> the cut array, and 
+        'cutdescription' -> a short message about how the cut was calculated.
 
  
 
+        Parameters
+        ----------
+            cutarr: ndarray
+                Array of bools
+            name: str
+                The name of cut to be saved
+            description: str
+                Very short description of how the cut was calculated
 
-def listcuts(whichcuts = 'current'):
-    """
-    Function to return all the available cuts saved in current_cuts/
-    or archived_cuts/
-    
-    Parameters
-    ----------
-        whichcuts: str, optional
-            String to specify which cuts to return. Can be 'current' or
-            'archived'. If 'current', only the cuts in the current_cuts/ 
-            directory are returned. If 'archived', the old cuts in the 
-            archived_cuts/ directory are returned
-        
-    Returns
-    -------
-        allcuts: list
-            List of names of all current cuts available
-            
-    Raises
-    ------
-        ValueError
-            If whichcuts is not 'current' or 'archived'
-            
-    """
-    
-    allcuts = []
-    path = os.path.dirname(os.path.abspath(__file__))
-    
-    
-    if whichcuts == 'current':
-        cutdir = 'current_cuts'
-    elif whichcuts == 'archived':
-        cutdir = 'archived_cuts'
-    else:
-        raise ValueError("Please select either 'current' or 'archived'")
-    
-    if not os.path.isdir(f'{path}/{cutdir}'):
-        print('No cuts have been generated yet')
+        Returns
+        -------
+            None
+
+        """
+        path = self.fullpath
+
+        # check if there is a current cut, then check if it has been changed
+        try:
+            ctemp = np.load(f'{path}/current_cuts/{name}.npz')['cut']
+
+            if np.array_equal(ctemp, cutarr):
+                print(f'cut: {name} is already up to date.')
+            else:
+                print(f'updating cut: {name} in directory: {path}/current_cuts/ and achiving old version')
+                np.savez_compressed(f'{path}/current_cuts/{name}', cut = cutarr, cutdescription=description)
+
+                files_old = glob(f'{path}/archived_cuts/{name}_v*')
+                if len(files_old) > 0:
+                    latestversion = int(sorted(files_old)[-1].split('_v')[-1].split('.')[0])
+                    version = int(latestversion +1)
+                else:
+                    version = 0
+                np.savez_compressed(f'{path}/archived_cuts/{name}_v{version}', cut = ctemp, cutdescription=description)
+                print(f'old cut is saved as: {path}/archived_cuts/{name}_v{version}.npz')
+
+        except FileNotFoundError:
+            print(f'No existing version of cut: {name}. \n Saving cut: {name}, to directory: {path}/current_cuts/')
+            np.savez_compressed(f'{path}/current_cuts/{name}', cut = cutarr, cutdescription=description)
+
         return
-    
-    files = glob(f'{path}/{cutdir}/*')
-    
-    if len(files) == 0:
-        print('No cuts have been generated yet')
-        return
-    else:
-        for file in files:
-            allcuts.append(file.split('/')[-1].split('.')[0])
-        return allcuts
-        
-def loadcut(name, lgccurrent = True):
-    """
-    Function to load a cut mask from disk into memory. The name should just be the 
-    base name of the cut, with no file extension. If an archived cut is desired, the 
-    version of the cut must be part of the name, i.e. 'cbase_v3'
-    
-    Parameters
-    ----------
-        name: str
-            The name of the cut to be loaded
-        lgccurrent: bool, optional
-            If True, the current cut with corresponding name is loaded,
-            if False, the archived cut is loaded
-    
-    Returns
-    -------
-        cut: ndarray
-            Array of booleans
-    
-    Raises
-    ------
-        FileNotFoundError
-            If the user specified cut cannot be loaded
-            
-    """
-    path = os.path.dirname(os.path.abspath(__file__))
-    
-    
-    if lgccurrent:
-        cutdir = 'current_cuts'
-    else:
-        cutdir = 'archived_cuts'
-    
-    
-    try:
-        cut = np.load(f'{path}/{cutdir}/{name}.npz')['cut']
-        return cut
-    except FileNotFoundError:
-        raise FileNotFoundError(f'{name} not found in {path}/{cutdir}/')
-        
-        
-    
-        
-def loadcutdescription(name, lgccurrent = True):
-    """
-    Function to load the description of a cut. The name should just be the 
-    base name of the cut, with no file extension. If an archived cut is desired, the 
-    version of the cut must be part of the name, i.e. 'cbase_v3'
-    
-    Parameters
-    ----------
-        name: str
-            The name of the cut to be loaded
-        lgccurrent: bool, optional
-            If True, the current cut with corresponding name is loaded,
-            if False, the archived cut is loaded
-    
-    Returns
-    -------
-        cutmessage: str
-            the description of the cut stored with the array
-    
-    Raises
-    ------
-        FileNotFoundError
-            If the user specified cut cannot be loaded
-            
-    """
-    path = os.path.dirname(os.path.abspath(__file__))
-    
-    
-    if lgccurrent:
-        cutdir = 'current_cuts'
-    else:
-        cutdir = 'archived_cuts'
-    
-    
-    try:
-        cutmessage = np.load(f'{path}/{cutdir}/{name}.npz')['cutdescription']
-        return cutmessage
-    except FileNotFoundError:
-        raise FileNotFoundError(f'{name} not found in {path}/{cutdir}/')    
-            
-        
-        
+
+
+
+
+    def listcuts(self, whichcuts = 'current'):
+        """
+        Function to return all the available cuts saved in current_cuts/
+        or archived_cuts/
+
+        Parameters
+        ----------
+            whichcuts: str, optional
+                String to specify which cuts to return. Can be 'current' or
+                'archived'. If 'current', only the cuts in the current_cuts/ 
+                directory are returned. If 'archived', the old cuts in the 
+                archived_cuts/ directory are returned
+
+        Returns
+        -------
+            allcuts: list
+                List of names of all current cuts available
+
+        Raises
+        ------
+            ValueError
+                If whichcuts is not 'current' or 'archived'
+
+        """
+
+        allcuts = []
+        path = self.fullpath
+
+
+        if whichcuts == 'current':
+            cutdir = 'current_cuts'
+        elif whichcuts == 'archived':
+            cutdir = 'archived_cuts'
+        else:
+            raise ValueError("Please select either 'current' or 'archived'")
+
+        if not os.path.isdir(f'{path}/{cutdir}'):
+            print('No cuts have been generated yet')
+            return
+
+        files = glob(f'{path}/{cutdir}/*')
+
+        if len(files) == 0:
+            print('No cuts have been generated yet')
+            return
+        else:
+            for file in files:
+                allcuts.append(file.split('/')[-1].split('.')[0])
+            return allcuts
+
+    def loadcut(self, name, lgccurrent = True):
+        """
+        Function to load a cut mask from disk into memory. The name should just be the 
+        base name of the cut, with no file extension. If an archived cut is desired, the 
+        version of the cut must be part of the name, i.e. 'cbase_v3'
+
+        Parameters
+        ----------
+            name: str
+                The name of the cut to be loaded
+            lgccurrent: bool, optional
+                If True, the current cut with corresponding name is loaded,
+                if False, the archived cut is loaded
+
+        Returns
+        -------
+            cut: ndarray
+                Array of booleans
+
+        Raises
+        ------
+            FileNotFoundError
+                If the user specified cut cannot be loaded
+
+        """
+        path = self.fullpath
+
+
+        if lgccurrent:
+            cutdir = 'current_cuts'
+        else:
+            cutdir = 'archived_cuts'
+
+
+        try:
+            cut = np.load(f'{path}/{cutdir}/{name}.npz')['cut']
+            return cut
+        except FileNotFoundError:
+            raise FileNotFoundError(f'{name} not found in {path}/{cutdir}/')
+
+
+
+
+    def loadcutdescription(self, name, lgccurrent = True):
+        """
+        Function to load the description of a cut. The name should just be the 
+        base name of the cut, with no file extension. If an archived cut is desired, the 
+        version of the cut must be part of the name, i.e. 'cbase_v3'
+
+        Parameters
+        ----------
+            name: str
+                The name of the cut to be loaded
+            lgccurrent: bool, optional
+                If True, the current cut with corresponding name is loaded,
+                if False, the archived cut is loaded
+
+        Returns
+        -------
+            cutmessage: str
+                the description of the cut stored with the array
+
+        Raises
+        ------
+            FileNotFoundError
+                If the user specified cut cannot be loaded
+
+        """
+        path = self.fullpath
+
+
+        if lgccurrent:
+            cutdir = 'current_cuts'
+        else:
+            cutdir = 'archived_cuts'
+
+
+        try:
+            cutmessage = np.load(f'{path}/{cutdir}/{name}.npz')['cutdescription']
+            return str(cutmessage)
+        except FileNotFoundError:
+            raise FileNotFoundError(f'{name} not found in {path}/{cutdir}/')    
+
+
+
         
         
         
